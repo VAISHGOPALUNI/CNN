1448ef083a43a86f497be2a6afc0851829a7ea55
VAISHNAVI GOPALUNI <42315342+VAISHGOPALUNI@users.noreply.github.com>
2020-07-16 18:02:19 +0530

    Add files via upload

---
+++ appie.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 26 17:31:16 2020
+
+@author: Abinaya S
+"""
+
+
+import numpy as np 
+from flask import Flask,request,render_template
+import os
+from werkzeug.utils import secure_filename
+app1 = Flask(__name__)
+from keras.models import load_model
+from keras.preprocessing import image
+import tensorflow as tf
+global graph
+graph = tf.get_default_graph()
+model = load_model("food.h5")
+@app1.route('/',methods = ["GET"] )
+def index():
+    return render_template("base.html")
+@app1.route('/predict',methods = ["GET","POST"])
+def upload():
+    if request.method == "POST":
+        f = request.files["image"]
+        basepath = os.path.dirname(__file__)
+        print("current path: ",basepath)
+        file_path = os.path.join(basepath,"uploads",secure_filename(f.filename))
+        f.save(file_path)
+        print("joined path: ",file_path)
+        img = image.load_img(file_path,target_size = (64,64))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x,axis = 0)
+        with graph.as_default():
+            preds = model.predict_classes(x)
+            print(preds)
+        index = ['cakes', 'chocolates', 'coffee', 'cookie', 'icecream']
+        text = "the predicted food item is : " +index[preds[0]]
+    return text 
+if __name__ == "__main__":
+    app1.run(debug = True)
